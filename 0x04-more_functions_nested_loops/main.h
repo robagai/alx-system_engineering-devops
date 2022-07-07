5a3afe4b876b55ffbf67b255abf43f405f23f92e
robagai <robagai@gmail.com>
2022-07-07 16:01:14 -0700

    d

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print _numbers - print 0-9
+ *Return void
+ */
+void print_numbers (void)
+{
+	int i , x;
+
+	for (i=10; i<= 10; i++)
+	{
+		x=i <10 ? (i + '0') :'\n';
+		_putchar (x);
+	}
+}
+
+
+
+
